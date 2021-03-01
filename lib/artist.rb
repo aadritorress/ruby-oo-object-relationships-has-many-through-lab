055d02d4b72758b2b4f55bb5d590bc7b9fedfe74
Adriana <adriana.amaral@msn.com>
2021-02-28 20:40:50 -0600

    commit tree 5ab0f71638e91cfd35d88e9ee6879e0f32c52868

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,34 @@
+class Artist 
 
+    attr_reader :name, :genre
+
+    @@all = []
+
+def initialize (name)
+    @name = name 
+    @genre = genre
+    @@all << self 
+end 
+
+def self.all
+    @@all
+end 
+
+def songs 
+    Song.all.select do |song|
+    song.artist == self 
+    end 
+end 
+
+
+def new_song(name, genre)
+    Song.new(name, self, genre)
+end
+
+def genres
+    self.songs.collect do |song|
+      song.genre
+    end
+  end
+
+end 
